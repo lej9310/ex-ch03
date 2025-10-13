0a7773adabc2c2ca535f3030679f38792afb7fe5
lej9310 <lej9310@gmail.com>
2025-10-13 16:24:15 +0900

    10/13

--- src/main/java/com/example/demo/controller/Mycontroller.java
+++ src/main/java/com/example/demo/controller/Mycontroller.java
@@ -47,9 +47,54 @@ public class Mycontroller {
 		return myService.ex73_while();
 	}
 	
+	// 다형성(polymorphism) - 상속 & 인터페이스
+	//인터페이스(부모 클래스)의 참조변수로 구현클래스(자식)의 인스턴스를 넣어 사용 가능
+	@GetMapping("/ex75_polymorphism")
+	public String ex75_polymorphism() {
+		System.out.println("====== ex75_다형성(polymorphism) ======");
+		return myService.ex75_polymorphism();
+	}
+	
+	// 인터페이스: 메서드 선언만 가지고 구현은 없음. 객체 생성 불가. 변수는 상수만 가능. 다중 구현 가능. implements로 구현체와 연계(List, Set, Map...)
+	// 구현체: 인터페이스를 구현하는 클래스. 메서드의 실제 동작 작성. 객체 생성 가능(ArrayList)
+	// ArrayList: List 인터페이스의 구현체. 배열처럼 여러개 데이터 다룸
+	@GetMapping("/ex76_ArrList")
+	public String ex76_ArrList() {
+		System.out.println("====== ex76_ArrayList ======");
+		return myService.ex76_ArrList();
+	}
+	
 	@GetMapping("/ex78_ArrList")
 	public String ex78_ArrList() {
 		System.out.println("====== ex78_ArrayList ======");
 		return myService.ex78_ArrList();
 	}
+	
+	// 동일성(identity)과 동등성(equality) 비교
+	@GetMapping("/ex79_iden_equal")
+	public String ex79_iden_equal() {
+		System.out.println("====== ex79_identity/equality ======");
+		return myService.ex79_iden_equal();
+	}
+	
+	// 람다 표현식(Lambdam Expression) >> 익명함수 
+	@GetMapping("/ex85_lambda")
+	public String ex85_lambda() {
+		System.out.println("====== ex85_lambda ======");
+		return myService.ex85_lambda();
+	}
+	
+	// 스트림 API
+	@GetMapping("/ex88_streamAPI")
+	public String ex88_streamAPI() {
+		System.out.println("====== ex88_streamAPI ======");
+		return myService.ex88_streamAPI();
+	}
+
+	@GetMapping("/ex89_streamAPI")
+	public String ex89_streamAPI() {
+		System.out.println("====== ex89_streamAPI ======");
+		return myService.ex89_streamAPI();
+	}
+	
 }
--- src/main/java/com/example/demo/service/MyService.java
+++ src/main/java/com/example/demo/service/MyService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,12 +17,17 @@ public class MyService {
 
 	public String sayhello(String hi) {
 		String msg = hi + ", 좋은 하루~!";
-		System.out.println(msg);
+
+		// System.out.println(msg);
 		Log.info(msg);
+//		Log.debug(msg);
+//		Log.warn(msg);
+//		Log.error(msg);
 
 		return msg;
 	}
 
+	// 3-2-1 ===========================================
 //	public String ex70_str() {
 //		String result = "";
 //		int num = 2;
@@ -42,7 +50,7 @@ public class MyService {
 			result = "else 블록입니다.";
 		}
 
-		System.out.println(result);
+		// System.out.println(result);
 		Log.info(result);
 
 		String msg = "ex70_str 결과: " + result;
@@ -54,41 +62,219 @@ public class MyService {
 		return 999;
 	}
 
+	// 3-2-2 ===========================================
 	public String ex72_for() {
 		int[] array = { 1, 2, 3, 4, 5 };
 		String result = "";
 
 		for (int i = 0; i < array.length; i++) {
 			String currentResult = "i = " + array[i];
-			result += currentResult + "\n";
+			result += currentResult + "<br>";
 
-			System.out.println(currentResult);
 			Log.info(currentResult);
 		}
 
-		String msg = "ex72_for 결과: " + result;
+		String msg = "ex72_for 결과: <br>" + result;
+		System.out.println(msg);
 		return msg;
 	}
 
+	// 3-2-3 ===========================================
 	public String ex73_while() {
-		StringBuilder stringBuilder = new StringBuilder();
+
 		int[] array = { 1, 2, 3, 4, 5 };
 		int i = 0;
+		String result = "";
 
 		while (i < array.length) {
 			String currentResult = "i = " + array[i];
-			stringBuilder.append(currentResult);
-
 			Log.info(currentResult);
+			result += currentResult + ", ";
 			i++;
 		}
 
-		String msg = "ex73_while 결과: " + stringBuilder.toString();
+		if (!result.isEmpty()) {
+			result = result.substring(0, result.length() - 2);
+		}
+
+		String msg = "ex73_while 결과: " + result;
+		System.out.println(msg);
 		return msg;
 	}
 
+	// 3-2-4 ===========================================
+	public interface Car {
+		public String getColor();
+	}
+
+	public class Sonata implements Car {
+		public Sonata() {
+			Log.info("Sonata 출고");
+		}
+
+		@Override
+		public String getColor() {
+			return "(Sonata) 색상: RED";
+		}
+	}
+
+	public class K5 implements Car {
+		public K5() {
+			Log.info("K5 출고");
+		}
+
+		@Override
+		public String getColor() {
+			return "(K5) 색상: YELLOW";
+		}
+	}
+
+	public String ex75_polymorphism() {
+		Car car1 = new Sonata();
+		String result1 = car1.getColor();
+		Car car2 = new K5();
+		String result2 = car2.getColor();
+
+		String msg = "차 2대 출고 완료<br>- " + result1 + "<br>- " + result2;
+		return msg;
+	}
+
+	// 3-2-5 ===========================================
+	// Java List 컬렉션
+	public String ex76_ArrList() {
+		// ArrayList<Integer>: ArrayList에 Integer 타입 저장
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		list.add(1);
+		list.add(2);
+		list.add(3);
+
+		Log.info("list 내용: " + list);
+
+		String msg = "ex76_ArrList<Integer> 결과: " + list;
+		System.out.println(msg);
+		return msg;
+	}
+
+	// 3-2-6 ===========================================
 	public String ex78_ArrList() {
-		
+
+		// ArrayList<String>: ArrayList에 String 타입 저장
+		List<String> list = new ArrayList<String>();
+		list.add("public");
+		list.add("static");
+		list.add("void");
+
+		// for문 >> list 요소 차례대로 출력
+		for (int i = 0; i < list.size(); i++) {
+			Log.info("list 내용: " + list.get(i));
+		}
+
+		// list의 두번째 요소 제거
+		list.remove(1);
+		System.out.println(list); // [public, void]
+
+		int voidIndex = list.indexOf("void"); // 1 반환
+
+		String msg = "void의 index = " + voidIndex;
+		System.out.println(msg);
+		return msg;
+	}
+
+	// 3-2-7 & 3-2-8 ===========================================
+	public String ex79_iden_equal() {
+
+		boolean result1;
+		boolean result2;
+
+		String str1 = new String("is same?");
+		String str2 = new String("is same?");
+
+		result1 = str1 == str2;
+		Log.info("str1과 str2가 같은가? " + result1); // false
+		result2 = str1.equals(str2);
+		Log.info("str1과 str2가 같은가? " + result2); // true
+
+		String msg1 = "동일성(identity) 결과: " + result1;
+		System.out.println(msg1);
+
+		String msg2 = "동등성(equality) 결과: " + result2;
+		System.out.println(msg2);
+
+		// 두 결과를 합친 문자열을 반환
+		return msg1 + "<br>" + msg2;
+	}
+
+	// 3-3-1 ===========================================
+
+	public String ex85_lambda() {
+		List<String> list = new ArrayList<String>();
+
+		list.add("public");
+		list.add("static");
+		list.add("void");
+
+		// 1. 익명 클래스 코드
+		// list.sort() 메소드: 리스트 요소를 정렬
+//		list.sort(new Comparator<String>() {
+//			@Override
+//			public int compare(String str1, String str2) {
+//				return str1.compareTo(str2);
+//			}
+//		});
+
+		// 2. 람다 표현식 코드
+		// list.sort((str1, str2) -> str1.compareTo(str2));
+		list.sort(Comparator.naturalOrder());
+
+		String msg = "ex85_lambda 결과: " + list;
+		System.out.println(msg);
+		return msg;
+	}
+
+	// 3-3-2 ===========================================
+	public String ex88_streamAPI() {
+		List<String> list = new ArrayList<String>();
+
+		list.add("public");
+		list.add("static");
+		list.add("void");
+
+		// 스트림 API: 컬렉션에 담긴 데이터 처리
+		// 스트림 API가 호출되는 컬렉션 객체의 요소들을 인자로 들어온 람다 표현식을 통해 처리
+
+		// 리스트 각 요소를 콘솔에 출력
+		// list.stream().forEach(str -> System.out.println(str));
+		list.stream().forEach(System.out::println);
+
+		String result = list.stream().collect(Collectors.joining("<br>"));
+
+		String msg = "ex88_streamAPI 결과: <br>" + result;
+		System.out.println(msg);
+		return msg;
+	}
+
+	// 3-3-3 ===========================================
+	public String ex89_streamAPI() {
+		// 1. Integer 배열 생성 및 List 변환
+		Integer[] integerArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		// Arrays.asList: 배열로부터 List 인터페이스 객체 생성
+		// 제네릭스<> 사용 >> 리스트의 타입 설정 >> String 들어가면 오류
+		List<Integer> list = Arrays.asList(integerArray);
+		List<Integer> evenList = new ArrayList<Integer>();
+
+		// 2. 스트림 API 활용: 짝수 필터링
+		for (int i = 0; i < list.size(); i++) {
+			Integer number = list.get(i);
+			if (number % 2 == 0) { // 2로 나눴을 때의 나머지가 0이면 2의 배수이다.
+				evenList.add(number);
+			}	
+		}		
+
+		for (int i = 0; i < evenList.size(); i++) {
+			System.out.println(evenList.get(i));
+		}
+
 		return null;
 	}
 
