a4afaa381791b83fd9403e1d2622bedddd1d88eb
lej9310 <lej9310@gmail.com>
2025-10-17 17:18:28 +0900

    10/17- properties

--- src/main/java/com/lej/ch06/controller/Ch06Controller.java
+++ src/main/java/com/lej/ch06/controller/Ch06Controller.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class Ch06Controller {
 
 	
-//	@GetMapping("/")
-//	public String chNum() {
-//		System.out.println("http://localhost:8090/ch06");
-//		return ch06Service.chNum();
-//	}
+	@GetMapping("/")
+	public String chNum() {
+		System.out.println("http://localhost:8090/ch06");
+		return "ch06 예시입니다.";
+	}
 
 	// 6-2-1. ==================================
-	@GetMapping("/article621-get")
-	public String createArticle(@RequestParam String title, @RequestParam String content) {
-//		String result = String.format("title=%s / content=%s", title, content);
-		String result = "제목: " + title + "<br>내용: " + content;
-		System.out.println("http://localhost:8090/ch06/article621-get");
-		return result;
-	}
+//	@GetMapping("/article621-get")
+//	public String createArticle(@RequestParam String title, @RequestParam String content) {
+////		String result = String.format("title=%s / content=%s", title, content);
+//		String result = "제목: " + title + "<br>내용: " + content;
+//		System.out.println("http://localhost:8090/ch06/article621-get");
+//		return result;
+//	}
 
 }
