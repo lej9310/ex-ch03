08c0a71ee2bc4dc1928d835057d8214ab42335f0
lej9310 <lej9310@gmail.com>
2025-10-17 17:33:20 +0900

    10/17

--- src/main/java/com/lej/tymleaf/contoroller/Mytymleaf.java
+++ src/main/java/com/lej/tymleaf/contoroller/Mytymleaf.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Mytymleaf {
 
-	// http://localhost:8080/hello
+	// http://localhost:8090/hello
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
 	}
 	
-	// http://localhost:8080/hello_thymleaf
+	// http://localhost:8090/hello_thymleaf
 	@GetMapping("/hello_thymleaf")
 	public String hello_thymleaf(Model model) {
 		model.addAttribute("message", "스프링부트 타임리프 화면");
 		return "hello_thymleaf";
 	}
 	
-	// http://localhost:8080/hello_mustache
+	// http://localhost:8090/hello_mustache
 	@GetMapping("/hello_mustache")
 	public String mustache(Model model) {
 		model.addAttribute("message", "스프링부트 머스테치 화면");
